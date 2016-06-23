5c58acb0be70182c9c67a3b46b13716c54f3a88d
Anderson Eduardo Jimenez Prieto <andersonmurdog@gmail.com>
2016-06-23 10:03:20 -0500

    Codificacion del hilo encargado de establecer laconexion https://github.com/anderson-0812/Bluetooth_activar_desactivar/issues/1

---
+++ app/src/main/java/com/myapplication/demo/bluetooth_activar_desactivar/BluetoothService.java
@@ -0,0 +1,68 @@
+package com.myapplication.demo.bluetooth_activar_desactivar;
+
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Handler;
+
+/**
+ * Created by USER on 22/06/2016.
+ *
+ * Crearemos la clase añadiéndole unas cuantas constantes para definir los posibles
+ estados y mensajes a manejar por el handler y un par de atributos privados para
+ almacenar el estado actual de la conexión, el socket de la conexión y el handler
+ que se encargará de comunicar los datos a la interfaz de usuario.
+ */
+public class BluetoothService {
+    private static final String TAG = "org.danigarcia.examples.bluetooth.BluetoothService";
+    public static final int ESTADO_NINGUNO = 0;
+    public static final int ESTADO_CONECTADO= 1;
+    public static final int ESTADO_REALIZANDO_CONEXION = 2;
+    public static final int ESTADO_ATENDIENDO_PETICIOPNES= 3;
+
+    public static final int MSG_LEER= 11;
+    public static final int MSG_ESCRIBIR= 12;
+
+    private static final Handler handler = null;
+    private BluetoothSocket socket;
+    private int estado;
+
+    // Hilo encargado de mantener la conexion y realizar las lecturas y escrituras
+    // de los mensajes intercambiados entre dispositivos.
+    private class HiloConexion extends Thread{
+        private final BluetoothSocket socket;
+        private final InputStream inputStream;// flujo de entrada (lecturas)
+        private final OutputStream outputStream;// flujo de salida (escrituras)
+
+        /*
+        El constructor recibirá un BluetoothSocket como parámetro, que será creado bien por el servidor
+        como respuesta a la aceptación de una conexión entrante, bien por el cliente como respuesta del
+        intento de conexión a un servidor
+        */
+        private HiloConexion(BluetoothSocket socket) {
+            this.socket = socket;
+            setName(socket.getRemoteDevice().getName()+ "[" + socket.getRemoteDevice().getAddress() + "]");
+            // Se usan variables temporales debido a que los atributos se declaran como final
+            // no seria posible asignarles valor posteriormente si fallara esta llamada
+            InputStream tmpInputStream = null;
+            OutputStream tmpOutputStream = null;
+
+            // Obtenemos los flujos de enyrada y salida del socket
+            try{
+                tmpInputStream = socket.getInputStream();
+                tmpOutputStream = socket.getOutputStream();
+            }catch (IOException e){
+                Log.e(TAG,"HiloConexion(): Error al obtener flujos de E/S",e);
+            }
+            inputStream = tmpInputStream;
+            outputStream = tmpOutputStream;
+
+        }
+
+
+    }
+
+}
